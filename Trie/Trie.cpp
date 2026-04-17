988e49de328ecf59ea8531c3dc5aef625a958b3f
Saugat Lamichhane <lamichhanesaugat62@gmail.com>
2026-04-16 21:45:58 -0500

    added Trie

---
+++ Trie/Trie.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+struct TrieNode {
+  int ends{0};
+  int startsWith{0};
+  std::unordered_map<char, TrieNode *> children;
+};
+
+class Trie {
+public:
+  Trie() : root(new TrieNode()) {}
+
+  void insert(std::string word) {
+    auto trie = root;
+    for (auto ch : word) {
+      if (trie->children.count(ch) == 0) {
+        trie->children[ch] = new TrieNode();
+      }
+      trie->startsWith++;
+      trie = trie->children[ch];
+    }
+    trie->ends++;
+    trie->startsWith++;
+  }
+
+  int countWordsEqualTo(std::string word) {
+    auto trie = root;
+    for (auto ch : word) {
+      if (trie->children.count(ch) == 0) {
+        return 0;
+      }
+      trie = trie->children[ch];
+    }
+    return trie->ends;
+  }
+
+  int countWordsStartingWith(std::string word) {
+    auto trie = root;
+    for (auto ch : word) {
+      if (trie->children.count(ch) == 0) {
+        return 0;
+      }
+      trie = trie->children[ch];
+    }
+    return trie->startsWith;
+  }
+
+  void erase(std::string word) {
+    auto trie = root;
+    for (auto ch : word) {
+      if (trie->children.count(ch) == 0) {
+        return;
+      }
+      trie->startsWith--;
+      trie = trie->children[ch];
+    }
+    trie->ends--;
+    trie->startsWith--;
+  }
+  ~Trie() { deleteTrie(root); }
+
+private:
+  void deleteTrie(TrieNode *node) {
+    for (auto &[ch, child] : node->children) {
+      deleteTrie(child);
+    }
+    delete node;
+  }
+  TrieNode *root;
+};
+
+int main() {
+  Trie trie;
+  trie.insert("apple");
+  std::cout << trie.countWordsEqualTo("apple") << "\n";
+  trie.insert("app");
+  std::cout << trie.countWordsStartingWith("app") << "\n";
+  trie.erase("apple");
+  std::cout << trie.countWordsStartingWith("app") << "\n";
+  return 0;
+}
